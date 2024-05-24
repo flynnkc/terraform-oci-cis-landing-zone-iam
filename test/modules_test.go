2aadb6fe874597e724a644c0feb0074a9975dceb
KC Flynn <38958974+flynnkc@users.noreply.github.com>
2024-05-24 14:24:58 -0400

    Identity Domains Tests

--- test/modules_test.go
+++ test/modules_test.go
@@ -9,6 +9,8 @@ import (
 // TestCompartments plans and applies terraform for the compartments module based on the
 // vision example.
 func TestCompartments(t *testing.T) {
+
+	// Init, plan, and apply
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
 		TerraformDir: "../compartments/examples/vision",
 		VarFiles:     []string{"input.auto.tfvars.template"},
@@ -17,13 +19,18 @@ func TestCompartments(t *testing.T) {
 
 	terraform.InitAndPlan(t, terraformOptions)
 
-	// We defer destroy, but this will leave compartments in place
 	defer terraform.Destroy(t, terraformOptions)
 	terraform.Apply(t, terraformOptions)
 
+	// Additional tests below
+
 }
 
+// TestDynamicGroups plans and applies terraform for the dynamic-groups module based
+// on the vision example.
 func TestDynamicGroups(t *testing.T) {
+
+	// Init, plan, and apply
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
 		TerraformDir: "../dynamic-groups/examples/vision",
 		VarFiles:     []string{"input.auto.tfvars.template"},
@@ -34,5 +41,27 @@ func TestDynamicGroups(t *testing.T) {
 
 	defer terraform.Destroy(t, terraformOptions)
 	terraform.Apply(t, terraformOptions)
+
+	// Additional tests below
+
+}
+
+// TestGroups plans and applies terraform for the groups module based on the visions
+// example.
+func TestGroups(t *testing.T) {
+
+	// Init, plan, and apply
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: "../groups/examples/vision",
+		VarFiles:     []string{"input.auto.tfvars.template"},
+		PlanFilePath: "./tf.plan",
+	})
+
+	terraform.InitAndPlan(t, terraformOptions)
+
+	defer terraform.Destroy(t, terraformOptions)
+	terraform.Apply(t, terraformOptions)
+
+	// Additional tests below
 
 }
